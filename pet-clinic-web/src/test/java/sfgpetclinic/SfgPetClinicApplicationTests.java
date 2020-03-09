9160df4f9de3e4e5d47e34aa8c768aca17a466c8
liguojing929 <dannyli0818@protonmail.com>
2020-03-09 15:47:55 +0800

    adding POJOs. Closes #2.

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,10 +1,10 @@
-package guru.springframework.sfgpetclinic;
+package sfgpetclinic;
 
+import guru.springframework.sfgpetclinic.SfgPetClinicApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@SpringBootTest(classes=SfgPetClinicApplication.class)
 class SfgPetClinicApplicationTests {
 
 	@Test
