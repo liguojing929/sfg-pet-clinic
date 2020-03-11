d9cc05cda11c0fe1ae42971a52017f4f47728a1a
liguojing929 <dannyli0818@protonmail.com>
2020-03-11 23:25:48 +0800

    Load Bootstrap Data on Startup. closes #14

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -8,7 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    /**
+     * @RequestMapping({"", "/", "/index",...}) means mapping different requests to the same method,
+     * which means type the url mentioned inside will return the same page.
+     * @return
+     */
+    @RequestMapping({"", "/", "/index", "/index.html", "/test"})
     public String listOwners() {
         return "owners/index";
     }
