ce46e753200b7f305b791e57633da1ace2646caa
liguojing929 <dannyli0818@protonmail.com>
2020-03-14 18:36:52 +0800

    Fixed the Broken Menu Links. closes #46.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class VetController {
 
@@ -15,9 +16,11 @@ public class VetController {
     }
 
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
+
+
 }
