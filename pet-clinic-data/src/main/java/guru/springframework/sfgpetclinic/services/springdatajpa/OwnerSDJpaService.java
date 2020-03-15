f2a92a33c09772fed51962a0aaf9417396055db4
liguojing929 <dannyli0818@protonmail.com>
2020-03-16 02:27:51 +0800

    Update Profiles to use Spring Data JPA Service. closes #59

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -12,7 +12,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Service
-@Profile("springDataJpa")
+@Profile("springdatajpa")
 public class OwnerSDJpaService implements OwnerService {
 
     private final OwnerRepository ownerRepository;
